655557c5ca2c197a0396e5d008c48bb269dee497
aselka27 <asjumalieva@gmail.com>
2023-03-03 23:24:14 +0600

    Initial Commit

---
+++ CollectionViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewDemo
+//
+//  Created by саргашкаева on 3.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
